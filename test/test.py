ca2640fbcbf4b7e240919f2c2877f0bea0b4fd56
Patrick McLaren <patrickkmclaren@gmail.com>
2014-11-04 21:08:40 -0500

    travis ci

---
+++ test/test.py
@@ -0,0 +1,10 @@
+from sage.all import *
+
+import unittest
+
+class TestFake(unittest.TestCase):
+    def test_fake(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
